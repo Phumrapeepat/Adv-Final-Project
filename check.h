bfe8b01af134b8b275c0a333d66b02c809e56b2b
Phumrapeepat <167950755+Phumrapeepat@users.noreply.github.com>
2024-07-01 14:52:55 +0000

    do

---
+++ check.h
@@ -0,0 +1,31 @@
+#ifndef check.h
+#define check.h
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int checknum (char s[])
+{
+    int i,n,a;
+    n=strlen(s);
+    for (i=0; i<n; i++)
+    {
+        if(s[i]!='0'&&s[i]!='1'&&s[i]!='2'&&s[i]!='3'&&s[i]!='4'&&s[i]!='5'&&s[i]!='6'&&s[i]!='7'&&s[i]!='8'&&s[i]!='9')
+            a=0;
+        return a;
+        else a=1;
+    }
+    return a;
+}
+
+int numin(){
+    string x;
+    cin<<x<<end;
+    if(checknum(x)==0){
+        cout<<"Please enter a proper number"<<endl;
+        return 0;
+    }
+    else{
+        return atoi(x);
+    }
+}
