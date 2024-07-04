a2ce0fa72a7bc356840c5800cff649fa739b89f8
Phumrapeepat <167950755+Phumrapeepat@users.noreply.github.com>
2024-07-04 15:22:27 +0000

    yeah

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@ using namespace std;
 #include <stdlib.h>
 #include "check.h"
 
+
 int main(){
     cout<<"Hello World"<<endl;
     pagebreak();
     cout<<"Sad"<<endl;
-    getch();
-   
 return 0;
 }
